807598857c08f36cf721eaf4eecf49f486af6fce
xHRanger <112560707+xHRanger@users.noreply.github.com>
2022-08-31 10:19:44 -0300

    anashe

--- src/inputs/divaslider.cpp
+++ src/inputs/divaslider.cpp
@@ -6,7 +6,10 @@
 
 bool DivaSliderInput::available() {}
 
-void DivaSliderInput::setup() {}
+void DivaSliderInput::setup() {
+  goio_init(PIN_LEFT_SLIDER_COM0);
+  gpio_set_dir(PIN_LEFT_SLIDER_COM0, GPIO_IN);
+}
 
 uint16_t DivaSliderInput::read() {
 
